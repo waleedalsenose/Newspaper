70cda4fb8ad6c1f4aeb00315587916f1e24233fa
Waleed <willy-alsenose@hotmail.com>
2022-12-04 15:41:23 +0200

    adjust allowed host

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://web-production-5050.up.railway.app']
 
 
 # Application definition
